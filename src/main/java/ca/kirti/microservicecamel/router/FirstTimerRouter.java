feced97d8a06996ee12d7eaa085f2b831ee7e566
kirtimistry2001 <kirtimistry2001@yahoo.com>
2021-04-03 13:32:17 -0400

    Updated to display dynamic(modified) message

--- src/main/java/ca/kirti/microservicecamel/router/FirstTimerRouter.java
+++ src/main/java/ca/kirti/microservicecamel/router/FirstTimerRouter.java
@@ -1,13 +1,18 @@
 package ca.kirti.microservicecamel.router;
 
+import java.time.LocalDateTime;
+
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 @Component
 public class FirstTimerRouter extends RouteBuilder{
 
+	@Autowired
+	private GetCurrentTimeBean getCurrentTimeBean;
 	@Override
 	public void configure() throws Exception {
-		//create routes
+		//create routes (https://www.youtube.com/watch?v=eh9C0GyxtHE&ab_channel=in28minutesCloud%2CDevOpsandMicroservices)
 		// listen to a queue/(timer and logs to save the message) , transformation(edit/update) and save to database
 		// from where you start the route, , the endpoint name  (e.g from timer endpoint. here 'from' is keyword 
 		//and "first-timer" is a nameof endpoint)
@@ -16,9 +21,18 @@ public class FirstTimerRouter extends RouteBuilder{
 		//between from and to you can do the things like transform the message
 		// Router 1 is from timer to log
 		from("timer:first-timer") //e.g you can use queue in place of timer in from endpoint
-		.transform().constant("Some constant default message")// here it picked up null message and modify it constant message you will get ----  [r://first-timer] first-timer : Exchange[ExchangePattern: InOnly, BodyType: String, Body: Some constant default message]
+		//.transform().constant("Some constant default message")// here it picked up null message and modify it constant message you will get ----  [r://first-timer] first-timer : Exchange[ExchangePattern: InOnly, BodyType: String, Body: Some constant default message]
+		//.transform().constant("Time now is:"+LocalDateTime.now())  // this will print the same time in each 
+		//.bean(getCurrentTimeBean) //this works when you have one method in your bean (e.g getCurrentTimeBean)
+		.bean(getCurrentTimeBean,"getCurrentTime") //use method name when you have more then 1 method in your bean
 		.to("log:first-timer"); //e.g. you can save it to database using to
 	}
 
 }
 
+@Component
+class GetCurrentTimeBean {
+	public String getCurrentTime() {
+		return "Time now is:"+LocalDateTime.now();
+	}
+}
