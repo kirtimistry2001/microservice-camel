1ac7c3206c8bf95cd9f0fce61d42656b9c830523
kirtimistry2001 <kirtimistry2001@yahoo.com>
2021-04-03 15:21:37 -0400

    Updated to put the message on ActiveMQ broker
    so the the other microservice (Received can receive it) To use this
    functionality need ActiveMQ. One must install the ActoveMQ on the
    machine

--- src/main/java/ca/kirti/microservicecamel/router/c/ActiveMQSenderRouter.java
+++ src/main/java/ca/kirti/microservicecamel/router/c/ActiveMQSenderRouter.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ActiveMQSenderRouter extends RouteBuilder{
-
+	//NOTE: you need ActiveMQ installed 
 	@Override
 	public void configure() throws Exception {
 		// create timer end point and another is queue endpoint
@@ -13,8 +13,11 @@ public class ActiveMQSenderRouter extends RouteBuilder{
 		// here every 10000 milisecond generating message and putting on the queue
 		from("timer:active-mq-timer?period=10000") // timer for 10000 miliseconds
 		.transform().constant("Test Messase for Active MQ")
-		//queue end point
+		.log("${body}")
+		
+		//queue end point ,used to put message on the queue so the other service (receiver can receive it)
 		.to("activemq:my-activemq-queue"); //'my-activemq-queue' is the queue name
+	
 	}
 
 }
