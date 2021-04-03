3edf98262123d55b126b0e2b2bb6d305c7586112
kirtimistry2001 <kirtimistry2001@yahoo.com>
2021-04-03 14:41:44 -0400

    Create two end points and moved/copied files
    from one end point to the another

--- src/main/java/ca/kirti/microservicecamel/router/b/FileRouter.java
+++ src/main/java/ca/kirti/microservicecamel/router/b/FileRouter.java
@@ -6,11 +6,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class FileRouter extends RouteBuilder{
 
+	
 	@Override
 	public void configure() throws Exception {
-		
-		from("file:files/input")
-		.to("file:files/output");
+		// on frist execution it will create folder 'files/input'
+		// add some files (e.g txt, csv , json files")
+		//after copy-pasting/ some files in 'input' folder rerun the application
+		// it will create 'files/output' folder copy all the files from input folder (1st end point) to the output folder (2nd end point)
+		from("file:files/input")  //first endpoint, It will create folder called 'files/input'
+		.log("${body}")	//processing , just printing body
+		.to("file:files/output"); //second enpoint
 	}
 
 }
